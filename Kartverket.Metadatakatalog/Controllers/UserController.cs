e82b8a8d47716f88e35ed54770f274d3bc26a94b
Ola Horg Jacobsen <ola.jacobsen@kartverket.no>
2026-08-25 12:25:16 +0200

    Basic endpoint

---
+++ Kartverket.Metadatakatalog/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Kartverket.Metadatakatalog.Service;
+using Kartverket.Metadatakatalog.Helpers;
+using Microsoft.AspNetCore.Cors;
+
+namespace Kartverket.Metadatakatalog.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class UserController(ILogger<HomeController> logger) : Controller
+    {
+        [HttpGet("user")]
+        [ProducesResponseType(typeof(UserResult), 200)]
+        public IActionResult GetUser()
+        {
+            logger.LogInformation("Get user called");
+            UserResult result = new UserResult("Navn Navnesen", "navn@eksempel.no");
+
+            return Ok(result);
+        }
+    }
+
+    public class UserResult
+    {
+        public UserResult(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+    }
+}
